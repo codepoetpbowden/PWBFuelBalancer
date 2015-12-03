a747c9f149cd0e41dbd64f9537c0c5064eeb27de
Philip Bowden <pbowden@codepoet.org.uk>
2015-12-03 21:48:26 +0000

    rebuilt against KSP 1.0.5

--- plugins/PWBFuelBalancer/Properties/AssemblyInfo.cs
+++ plugins/PWBFuelBalancer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PWB")]
 [assembly: AssemblyProduct("PWBFuelBalancer")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.0.7.0")]
-[assembly: AssemblyFileVersion("0.0.7.0")]
+[assembly: AssemblyVersion("0.1.2.0")]
+[assembly: AssemblyFileVersion("0.1.2.0")]
